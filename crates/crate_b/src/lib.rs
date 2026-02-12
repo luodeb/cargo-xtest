0745234a61b5c94402f48489745e23670878720a
Debin <luodebin@kylinos.cn>
2026-02-12 13:30:17 +0800

    rustc-wrapper

--- crates/crate_b/src/lib.rs
+++ crates/crate_b/src/lib.rs
@@ -3,4 +3,6 @@ pub fn crate_b_main() {
     println!("Crate B (smp)");
     #[cfg(not(feature = "smp"))]
     println!("Crate B (unsmp)");
+    #[cfg(feature = "smp")]
+    crate_smp::crate_smp_main();
 }
